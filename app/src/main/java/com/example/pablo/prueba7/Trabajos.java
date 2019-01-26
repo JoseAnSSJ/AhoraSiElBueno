4cbb49d5576d9e4fa7582c135c43795b95132eaa
brayan reyes <brayanhuy19@gmail.com>
2019-01-26 13:54:33 -0600

    Todo Funcional

--- app/src/main/java/com/example/pablo/prueba7/Trabajos.java
+++ app/src/main/java/com/example/pablo/prueba7/Trabajos.java
@@ -13,6 +13,7 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.pablo.prueba7.Listas.Array;
@@ -26,7 +27,11 @@ import com.example.pablo.prueba7.Request.Request;
  */
 public class Trabajos extends Fragment {
 
+    RelativeLayout layoutAnimado;
     Request request = new Request();
+    public static TextView trabajo;
+    public static Button accion;
+
     public Trabajos() {
         // Required empty public constructor
     }
@@ -37,6 +42,9 @@ public class Trabajos extends Fragment {
         View view = inflater.inflate(R.layout.fragment_trabajos, container, false);
 
         ListView trabajos = view.findViewById(R.id.listTrabajos);
+        layoutAnimado = view.findViewById(R.id.mostrar1);
+
+        //  contenedorCorporativo = view.findViewById(R.id.mostr2);
 
 
         TrabajosAdapter trabadapt = new TrabajosAdapter();
@@ -66,67 +74,63 @@ public class Trabajos extends Fragment {
         }
 
 
-        public class Holder {
-            TextView trabajo;
-            Button accion;
 
-        }
 
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            final Holder holder = new Holder();
 
-            if (convertView == null) {
+
+
                 convertView = getLayoutInflater().inflate(R.layout.list_trabajos_items, null);
-                holder.trabajo = convertView.findViewById(R.id.trabajov);
-                holder.accion = (Button) convertView.findViewById(R.id.accionv);
-            }
+                  trabajo = convertView.findViewById(R.id.trabajov);
+                   accion = (Button) convertView.findViewById(R.id.accionv);
 
 
-            holder.trabajo.setText(Array.trabajox.get(position));
-            holder.accion.setText(Array.accionx.get(position));
 
-                holder.accion.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if ((holder.trabajo.getText().toString().trim()).equalsIgnoreCase("DESCO - Desconexión Del Servicio Principal")) {
-                            Intent intento1 = new Intent(getContext(), Inicio.class);
-                            startActivity(intento1);
-                        }
-                        if ((holder.trabajo.getText().toString().trim()).equalsIgnoreCase("ISNET - Instalación de Servicio de Internet")) {
-                            Intent intento = new Intent(getContext(), CambioAparato.class);
-                            startActivity(intento);
+            trabajo.setText(Array.trabajox.get(position));
+            accion.setText(Array.accionx.get(position));
+            accion.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if ((trabajo.getText().toString().trim()).equalsIgnoreCase("DESCO - Desconexión Del Servicio Principal")) {
+                        accion.setEnabled(false);
 
-                        }
-                        if ((holder.trabajo.getText().toString().trim()).equalsIgnoreCase("ISTVA - Instalación de Servicio de TV")) {
-                            Intent intento = new Intent(getContext(), asignacion.class);
-                            startActivity(intento);
-                        }
-                        if ((holder.trabajo.getText().toString().trim()).equalsIgnoreCase("CAPAG - Cambio de tipo de aparato  FTTH")) {
-                            Intent intento = new Intent(getActivity(), CambioAparato.class);
-                            startActivity(intento);
-                        }
-                        if ((holder.trabajo.getText().toString().trim()).equalsIgnoreCase("CAMDO - Cambio De Domicilio")) {
-                            request.getCAMDO();
-                            Intent intento = new Intent(getActivity(), CambioDom.class);
-                            startActivity(intento);
-                            }
-                           if ((holder.trabajo.getText().toString().trim()).equalsIgnoreCase("CAPAT - Cambio De Tipo De Aparato")){
-                               Intent intento = new Intent(getActivity(), CambioAparato.class);
-                               startActivity(intento);
+                      if ((accion.getText().toString().trim()).equalsIgnoreCase("null")){
+                          accion.setText("---");
+
 
                         }
                     }
+                    if ((trabajo.getText().toString().trim()).equalsIgnoreCase("ISNET - Instalación de Servicio de Internet")) {
+                        Intent intento = new Intent(getContext(), CambioAparato.class);
+                        startActivity(intento);
+
+                    }
+                    if ((trabajo.getText().toString().trim()).equalsIgnoreCase("ISTVA - Instalación de Servicio de TV")) {
+                        Intent intento = new Intent(getContext(), asignacion.class);
+                        startActivity(intento);
+                    }
+                    if ((trabajo.getText().toString().trim()).equalsIgnoreCase("CAPAG - Cambio de tipo de aparato  FTTH")) {
+                        Intent intento = new Intent(getActivity(), CambioAparato.class);
+                        startActivity(intento);
+                        }
+                    if ((trabajo.getText().toString().trim()).equalsIgnoreCase("CAMDO - Cambio De Domicilio")) {
+                        request.getCAMDO();
+                        Intent intento = new Intent(getActivity(), CambioDom.class);
+                        startActivity(intento);
+                    }
+                    if ((trabajo.getText().toString().trim()).equalsIgnoreCase("CAPAT - Cambio De Tipo De Aparato")) {
+                        Intent intento = new Intent(getActivity(), CambioAparato.class);
+                        startActivity(intento);
+
+                    }
+                }
 
 
+            });
 
-                });
-
-                return convertView;
+            return convertView;
         }
     }
 }
-
-
-
